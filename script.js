eea8dc681e554e51e2f71ca9a71e7b68a0e7b833
StilCyber <karnozavr666@ya.ru>
2022-10-12 13:19:42 +0300

    obj

--- script.js
+++ script.js
@@ -334,3 +334,206 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+
+function caseInSwitch(val) {
+   let answer = "";
+   // Only change code below this line
+   switch (val) {
+     case (1):
+       answer = "alpha";
+       break;
+     case (2):
+       answer = "beta";
+       break;
+     case (3):
+       answer = "gamma";
+       break;
+     case (4):
+       answer = "delta";
+   }
+ 
+ 
+   // Only change code above this line
+   return answer;
+ }
+ 
+ caseInSwitch(1);
+
+ function switchOfStuff(val) {
+   let answer = "";
+   // Only change code below this line
+   switch (val) {
+     case "a":
+       answer = "apple";
+       break;
+     case "b":
+       answer = "bird";
+       break;
+     case "c":
+       answer = "cat";
+       break;
+     default:
+       answer = "stuff";
+   }
+ 
+ 
+   // Only change code above this line
+   return answer;
+ }
+ 
+ switchOfStuff(1);
+
+ function sequentialSizes(val) {
+   let answer = "";
+   // Only change code below this line
+ 
+   switch (val) {
+     case 1:
+     case 2:
+     case 3:
+       answer = "Low";
+       break;
+     case 4:
+     case 5:
+     case 6:
+       answer = "Mid";
+       break;
+     case 7:
+     case 8:
+     case 9:
+       answer = "High";
+   }
+ 
+   // Only change code above this line
+   return answer;
+ }
+ 
+ sequentialSizes(1);
+
+ function chainToSwitch(val) {
+   let answer = "";
+   // Only change code below this line
+ 
+ 
+ 
+   switch (val) {
+     case "bob":
+       answer = "Marley";
+       break;
+     case 42:
+       answer = "The Answer";
+       break;
+     case 1:
+       answer = "There is no #1";
+       break;
+     case 99:
+       answer = "Missed me by this much!";
+       break;
+     case 7:
+       answer = "Ate Nine";
+ 
+   }
+ 
+   // Only change code above this line
+   return answer;
+ }
+ 
+ chainToSwitch(7);
+
+ function isLess(a, b) {
+   // Only change code below this line
+   return a < b;
+   // Only change code above this line
+ }
+ 
+ isLess(10, 15);
+
+ // Setup
+function abTest(a, b) {
+   // Only change code below this line
+   if (a < 0) {
+     return undefined;
+   }
+   if (b < 0) {
+     return undefined;
+   }
+ 
+ 
+   // Only change code above this line
+ 
+   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+ }
+ 
+ abTest(2,2);
+
+ let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card) {
+    switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        count++; 
+        break;
+      case 10:
+      case "J":
+      case "Q":
+      case "K":
+      case "A":
+        count--;   
+  } 
+
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+
+  // return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+
+    const alpha = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+result = alpha[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+function checkObj(obj, checkProp) {
+   // Only change code below this line
+ 
+ 
+ 
+   if (obj.hasOwnProperty(checkProp)) {
+     return obj[checkProp];
+   } else {
+     return "Not Found";
+   };
+ 
+ 
+ 
+ 
+   // Only change code above this line
+ }
