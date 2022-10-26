c8a339df8c1b3eb2061ba1a2a27f69237e262de7
StilCyber <karnozavr666@ya.ru>
2022-10-26 12:52:37 +0300

    style

--- script.js
+++ script.js
@@ -537,3 +537,51 @@ function checkObj(obj, checkProp) {
  
    // Only change code above this line
  }
+
+ const myArray = [];
+ for ( let i = 9; i > 0; i -= 2) {
+   myArray.push(i);
+ }; 
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total = total + myArr[i];
+};
+
+function multiplyAll(arr) {
+   let product = 1;
+   // Only change code below this line
+   for (let i = 0; i < arr.length; i++) {
+     for (let j = 0; j < arr[i].length; j++) {
+       product = product * arr[i][j];
+       console.log(product);
+     }
+   } 
+   // Only change code above this line
+   return product;
+ }
+ 
+ multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+ // Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+  console.log(i);
+ } while (i < 5) {
+
+}
+
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0) {
+    return 0;
+  } else {
+      return sum(arr, n - 1) + arr[n - 1];
+  }
+}
